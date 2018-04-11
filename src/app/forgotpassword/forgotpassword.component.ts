ed4d9dc9d7d458ae35fab0186511236ff4250f0b
SanaShaikh28 <SanaShaikh28>
2018-04-11 18:48:39 +0530

    forgot password component

---
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-forgotpassword',
+  templateUrl: './forgotpassword.component.html',
+  styleUrls: ['./forgotpassword.component.css']
+})
+export class ForgotpasswordComponent implements OnInit {
+
+  model:any={};
+  constructor(private userservice:UserService) { }
+
+  ngOnInit() {
+  }
+
+/* Forgotpassword Api*/
+forgot():void
+{
+  console.log(this.model);
+  this.userservice.postservice('forgetpassword',this.model).subscribe(data=>console.log(data));
+}
+}
