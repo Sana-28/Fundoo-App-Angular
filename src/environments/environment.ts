2dd9d9075fcf120c4cd80361c2597bb1633004bd
SanaShaikh28 <SanaShaikh28>
2018-05-25 17:30:43 +0530

    Modified environment as pr spring boot

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  base_url:"http://localhost:8080/ToDo-App/"
+  //base_url:"http://localhost:8080/ToDo-App/"
+  base_url:"http://localhost:8080/"
 };
