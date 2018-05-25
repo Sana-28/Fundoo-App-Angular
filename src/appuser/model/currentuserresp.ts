df43155c81ad982f1100a49ce16e2ca2a3c73aeb
SanaShaikh28 <SanaShaikh28>
2018-05-25 17:32:07 +0530

    Modified app to appuser and all components and service

---
+++ src/appuser/model/currentuserresp.ts
@@ -0,0 +1,11 @@
+export class CurrentUserResp {
+
+    userId   : number;
+    name     : string;
+    email    : string;
+    password : string;
+    mobileNumber : string;
+    status   : string;
+    token    : string;
+   
+  }
