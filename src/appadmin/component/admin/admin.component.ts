1f1b9a58a576c426342a65ca0bfbba5bc96a019f
SanaShaikh28 <SanaShaikh28>
2018-06-05 11:50:54 +0530

    modified admin login

--- src/appadmin/component/admin/admin.component.ts
+++ src/appadmin/component/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../service/login.service';
+import { AdminResp } from '../../model/adminresp';
 
 @Component({
   selector: 'app-admin',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  model: any = {};
-  constructor() { }
+  model : any = {};
+  adminresp : AdminResp[];
+  constructor(private loginServiceObj: LoginService) { }
 
   ngOnInit() {
   }
 
+  /**@method:This method is to call login APi */
+  login(): void {
+    //debugger;
+    console.log("loginForm", this.model);
+    this.loginServiceObj.login(this.model)
+                          .subscribe(response =>              
+                          {
+                            console.log("loginSuccess..");
+                        });
+  }
 }
