ee451e4b442b4570c8d226031d7ab32ccb49687c
SanaShaikh28 <SanaShaikh28>
2018-06-05 11:51:54 +0530

    modified admin module

--- src/appadmin/appadmin.module.ts
+++ src/appadmin/appadmin.module.ts
@@ -4,21 +4,18 @@ import { AdminComponent } from './component/admin/admin.component';
 import { AdminService } from './service/admin.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarComponent }  from './component/calendar/calendar.component';
-// import { ChartsModule } from 'ng-charts';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { GraphService } from './service/graph.service';
+import { DatabaseComponent } from './component/database/database.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HomeComponent } from './component/home/home.component';
+import { LoginService } from './service/login.service';
 import { MapComponent } from './component/map/map.component';
 import { MaterialModule } from '../appadmin/material.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TimelineComponent } from './component/timeline/timeline.component';
 import { UserNoteService } from './service/usernote.service';
-import { ChartsModule } from 'ng2-charts';
-
 
 // import { AppRoutingModule } from '../appadmin/app-routing.module';
 
@@ -27,8 +24,8 @@ import { ChartsModule } from 'ng2-charts';
   declarations: [
     AppAdminComponent,
     AdminComponent,
-    CalendarComponent,
     DashboardComponent,
+    DatabaseComponent,
     HomeComponent,
     MapComponent,
     TimelineComponent
@@ -38,7 +35,6 @@ import { ChartsModule } from 'ng2-charts';
     //AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    ChartsModule,
     MaterialModule,
     RouterModule,
     HttpClientModule,
@@ -52,7 +48,7 @@ import { ChartsModule } from 'ng2-charts';
                               { path:'dashboard', component:DashboardComponent},
                               { path:'map', component: MapComponent},
                               { path:'timeline', component: TimelineComponent},
-                              { path:'calendar', component:CalendarComponent},
+                              { path: 'database', component: DatabaseComponent}
                             ]
                           },
                           ]),
@@ -61,7 +57,7 @@ import { ChartsModule } from 'ng2-charts';
  entryComponents:[],
 
   providers: [AdminService,
-                GraphService,
+                LoginService,
                   UserNoteService
                ],
 
